7a23d53ca672d62218f7ccbbcb592e48df17637b
黃志仁 <tbsthitw@gmail.com>
2019-10-03 00:50:53 +0800

    Decrypt function done.

---
+++ hw1/Decrypt.py
@@ -0,0 +1,68 @@
+#!/usr/local/bin/python3
+
+import sys
+from string import ascii_uppercase as uppercase
+from textwrap import wrap
+from itertools import cycle
+
+
+def die(message):
+    print(message)
+    sys.exit(0)
+
+
+len(sys.argv) == 4 or die("Usage: ./decrypt.py [cipher] [key] [ciphertext]")
+
+cipher, key, ciphertext = sys.argv[1:]
+
+ciphertext = ciphertext.upper()
+key = key.upper()
+
+if cipher == 'caesar':
+    key.isnumeric() or die("key should be integer for caesar cipher.")
+    key = int(key)
+    plaintext = ''.join([chr((ord(c) - 65 - key) % 26 + 65) if c in uppercase else c for c in ciphertext])
+
+elif cipher == 'playfair':
+    seen = set()
+    key = key + uppercase
+    key = ''.join([c for c in key if c in uppercase and not (c in seen or seen.add(c))])
+    key = key.replace('I' if key.rindex('I') > key.rindex('J') else 'J', '')
+    key_matrix = wrap(key, 5)
+    plaintext = ""
+    for (a, b) in zip(ciphertext[::2], ciphertext[1::2]):
+        i_a, i_b = key.index(a), key.index(b)
+        x_a, y_a = i_a // 5, i_a % 5
+        x_b, y_b = i_b // 5, i_b % 5
+        if y_a == y_b:
+            plaintext += key_matrix[(x_a - 1) % 5][y_a] + key_matrix[(x_b - 1) % 5][y_b]
+        elif x_a == x_b:
+            plaintext += key_matrix[x_a][(y_a - 1) % 5] + key_matrix[x_b][(y_b - 1) % 5]
+        else:
+            plaintext += key_matrix[x_a][y_b] + key_matrix[x_b][y_a]
+
+elif cipher == 'vernam':
+    plaintext = ''.join([chr(((ord(c) - 65) ^ (ord(k) - 65)) + 65) for c, k in zip(ciphertext, key)])
+    for i, c in enumerate(ciphertext[len(key):]):
+        plaintext += chr(((ord(c) - 65) ^ (ord(plaintext[i]) - 65)) + 65)
+
+elif cipher == 'row':
+    key.isnumeric() or die("key should be integer for row transposition cipher.")
+    groups = len(ciphertext) // len(key)
+    ciphertext = wrap(ciphertext, len(ciphertext) // len(key))
+    plaintext = ''.join([''.join([ciphertext[int(k)-1][i] for k in key]) for i in range(groups)])
+
+elif cipher == 'rail_fence':
+    key.isnumeric() or die("key should be integer for row transposition cipher.")
+    key = int(key)
+    r = list(range(key))
+    pattern = cycle(r + r[-2:0:-1])
+    indexes = sorted(range(len(ciphertext)), key=lambda i: next(pattern))
+    result = [''] * len(ciphertext)
+    for i, c in zip(indexes, ciphertext): result[i] = c
+    plaintext = ''.join(result)
+
+else:
+    die("Unknown cipher: " + cipher)
+
+print(plaintext.lower())
